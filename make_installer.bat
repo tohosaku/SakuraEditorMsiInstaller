a2800542e9c7ecdf8670e39997aba48e626d100d
tohosaku <ny@cosmichorror.org>
2012-01-15 10:34:57 +0900

    パスを追加(32bit版OSでも動作するように)

--- make_installer.bat
+++ make_installer.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 
 SET PATH=%PATH%;"C:\Program Files (x86)\Windows Installer XML v3.5\bin"
+SET PATH=%PATH%;"C:\Program Files\Windows Installer XML v3.5\bin"
 
 if exist sakura.msi    del /Q sakura.msi
 if exist sakura.wixobj del /Q sakura.wixobj
